7c2ebcf5e4d8adbbcb22353d67e9131a6bc04d41
DJ Jaiswal <digvijays18@gmail.com>
2018-02-10 23:01:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(arr_of_colors)
-  
+  puts "R: #{arr_of_colors[0]}"
+  puts "O: #{arr_of_colors[1]}"
+  puts "Y: #{arr_of_colors[2]}"
+  puts "G: #{arr_of_colors[3]}"
+  puts "B: #{arr_of_colors[4]}"
+  puts "I: #{arr_of_colors[5]}"
+  puts "V: #{arr_of_colors[6]}"
 end
